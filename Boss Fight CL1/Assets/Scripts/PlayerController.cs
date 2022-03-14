f2b99279b45b32bf099f4e6139005d7f56f0f1ad
Akshay Bharadhwaj <akshaybharadhwaj98@gmail.com>
2022-03-14 04:51:59 -0400

    Commented on code

--- Boss Fight CL1/Assets/Scripts/PlayerController.cs
+++ Boss Fight CL1/Assets/Scripts/PlayerController.cs
@@ -15,12 +15,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Moves the player character along a direction
         Move(KeyCode.W, 0, moveSpeed);
         Move(KeyCode.S, 0, -moveSpeed);
         Move(KeyCode.A, -moveSpeed, 0);
         Move(KeyCode.D, moveSpeed, 0);
     }
 
+    //function to move the player character
     void Move(KeyCode key, float xMove, float yMove)
     {
         if(Input.GetKey(key))
