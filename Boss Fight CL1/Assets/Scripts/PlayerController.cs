d68a8b67a2248de5478d8dd1da43d3a2bc850c5c
Akshay Bharadhwaj <akshaybharadhwaj98@gmail.com>
2022-03-28 23:58:59 -0400

    Added new assets and set up tilemaps

--- Boss Fight CL1/Assets/Scripts/PlayerController.cs
+++ Boss Fight CL1/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public int health = 4;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullheart;
+    public Sprite noheart;
+
+    public Rigidbody2D rb;
+    Vector2 mousePos;
+    public Camera cam;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +32,40 @@ public class PlayerController : MonoBehaviour
         Move(KeyCode.A, -moveSpeed, 0);
         Move(KeyCode.D, moveSpeed, 0);
 
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if(health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < health)
+            {
+                hearts[i].sprite = fullheart;
+            }
+            else
+            {
+                hearts[i].sprite = noheart;
+            }
+            if(i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+
+        }
+    }
+
+    private void FixedUpdate() 
+    {
+        Vector2 lookDir = mousePos - rb.position;               //vector to find the direction the mouse is pointing
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;        //mathf function to find the angle and set the Z-axis rotation
+        rb.rotation = angle;
     }
 
     //function to move the player character
